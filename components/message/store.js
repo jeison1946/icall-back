85f41e42842806d8843db899f7564745d422d94c
Json Jimenez <jeison@jeison.com>
2023-04-27 14:14:48 -0500

    update

--- components/message/store.js
+++ components/message/store.js
@@ -21,24 +21,9 @@ async function getMessage(filterChat) {
         resolve(populated);
       }) */
 
-    /* Model.model.aggregate([
-      { $match: filters },
-      { $group: { _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } }, messages: { $push: "$$ROOT" } } },
-      { $lookup: { from: 'users', localField: 'messages.user', foreignField: '_id', as: 'users' } },
-      { $sort: { "_id": -1 } }
-    ])
-    .exec((error, populated) => {
-      if(error) {
-        reject(error)
-      }
-      resolve(populated);
-    }) */
     Model.model.aggregate([
       { $match: filters },
       { $lookup: { from: 'users', localField: 'user', foreignField: '_id', as: 'userLoad' } },
-/*       { $set: {
-        "sass": "$user"
-      }}, */
       {$unwind: '$userLoad'},
       { $group: { _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } }, messages: { $push: "$$ROOT" } } },
     ])
